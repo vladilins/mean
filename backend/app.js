899dabec79d7b4cbce9c6991d2d1e8eb225e216e
vladilin <ilin96inbox@gmail.com>
2019-03-21 19:58:03 +0300

    node route /api/posts

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  next();
-});
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "sadfsdf",
+      title: "First server post",
+      content: "THis is from the server"
+    },
+    {
+      id: "sadweq1fsdf",
+      title: "Second server post",
+      content: "THis is from the server"
+    }
+  ];
 
-app.use((req, res, next) => {
-  res.send("Hello");
+  res.status(200).json({ message: "Post succe", posts: posts });
 });
 
 module.exports = app;
