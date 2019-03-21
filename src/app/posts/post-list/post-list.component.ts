7fa81e4ba1d0b5f22ca25a8d8fad6acb9aab5e42
vladilin <ilin96inbox@gmail.com>
2019-03-21 19:52:56 +0300

    added node js

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -18,7 +18,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   private postsSub: Subscription;
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService) { }
 
   ngOnInit() {
     this.posts = this.postsService.getPosts();
