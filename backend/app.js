70a4b9be6eab63f314b2d41582b14606870b1354
vladilin <ilin96inbox@gmail.com>
2019-03-22 11:46:55 +0300

    fetched posts from Db

--- backend/app.js
+++ backend/app.js
@@ -43,21 +43,10 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "sadfsdf",
-      title: "First server post",
-      content: "THis is from the server"
-    },
-    {
-      id: "sadweq1fsdf",
-      title: "Second server post",
-      content: "THis is from the server"
-    }
-  ];
-
-  res.status(200).json({ message: "Post succe", posts: posts });
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({ message: "Post fetched", posts: documents });
+  });
 });
 
 module.exports = app;
