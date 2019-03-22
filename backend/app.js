55ee327539d3dd8c2ee5c13a0abd34b711523f99
vladilin <ilin96inbox@gmail.com>
2019-03-22 11:27:37 +0300

    adde post model to node js post methond

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Post = require("./models/post");
 
 const app = express();
 
@@ -20,7 +21,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added success"
