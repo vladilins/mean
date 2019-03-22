8aeac112db09db9bdec6d443672039cb3837efe3
vladilin <ilin96inbox@gmail.com>
2019-03-22 15:55:36 +0300

    added update post methods

--- backend/app.js
+++ backend/app.js
@@ -45,6 +45,18 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Update success" });
+  });
+});
+
 app.get("/api/posts", (req, res, next) => {
   Post.find().then(documents => {
     res.status(200).json({ message: "Post fetched", posts: documents });
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,6 +36,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return { ...this.posts.find(p => p.id === id) };
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { title: title, content: content, id: null };
     this.http
@@ -48,6 +52,15 @@ export class PostsService {
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        console.log(response);
       });
   }
 
